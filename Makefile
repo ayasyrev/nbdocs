70c1f952a86eaf32c59f98a475ebce03a0c3f9fe
ayasyrev <a.yasyrev@gmail.com>
2022-02-09 10:02:44 +0300

    inital

---
+++ Makefile
@@ -0,0 +1,11 @@
+.ONESHELL:
+SHELL := /bin/bash
+
+pypi: dist
+	twine upload --repository pypi dist/*
+
+dist: clean
+	python setup.py sdist bdist_wheel
+
+clean:
+	rm -rf dist
---
+++ noxfile.py
@@ -0,0 +1,18 @@
+import nox
+
+
+@nox.session(python=["3.8", "3.9", "3.7"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.install(".", "pytest", "pytest-cov", "coverage[toml]")
+    session.run("pytest", *args)
+
+
+locations = "nbdocs", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8", "3.9", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
